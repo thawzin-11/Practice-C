8bcfe775f782a4bf098e32a336043bc540e8dece
thawzin-11 <skydark.thawzintun@gmail.com>
2020-09-08 17:56:14 +0630

    Format Specifiers

--- main.c
+++ main.c
@@ -3,78 +3,18 @@
 
 int main()
 {
+    int integerVar = 100;
+    float floatingVar = 331.79;
+    double doubleVar = 8.44e+11;
+    char charVar = 'W';
+    _Bool boolVar = 0;
 
-    char characterName[] = "John";
-    int characterAge = 67;
+    printf("integerVar = %i floatingVar = %f\n", integerVar, floatingVar);
+    printf("floatingVar = %f\n", floatingVar);  //2.f => 2 decimal point
+    printf("doubleVar = %e\n", doubleVar);
+    printf("charVar = %c\n", charVar);
+    printf("boolVar = %i\n", boolVar);
 
-    printf("There once was a man named %s\n", characterName);
-    printf("he was %d years old.\n", characterAge);
-    printf("He really liked the name %s\n", characterName);
-    printf("but he did not like being %d\n", characterAge);
-    return 0;
-}
-
-int datatype()
-{
-    int age = 40;
-    double gpa = 3.6;
-    char grade = 'A';
-    char phrase[] = "Giraffe Academy";
-}
-
-int sampleVariables()
-{
-    /* This is a sample program that lists three kids and their school
-    supply needs, as well as cost to buy the supplies */
-    // Set up the variables, as well as define a few
-
-    char firstInitial, middleInitial;
-    int number_of_pencils;
-    int number_of_notebooks;
-    float pencils = 0.23;
-    float notebooks = 2.89;
-    float lunchbox = 4.99;
-
-    //The information for the first child
-    firstInitial = 'J';
-    middleInitial = 'R';
-
-    number_of_pencils = 7;
-    number_of_notebooks = 4;
-
-    printf("%c%c needs %d pencils, %d notebooks, and 1 lunchbox\n",
-           firstInitial, middleInitial,number_of_pencils,
-           number_of_notebooks);
-    printf("The total cost is $%.2f\n\n", number_of_pencils*pencils
-           + number_of_notebooks*notebooks + lunchbox);
-
-    //The information for the second child
-    firstInitial = 'A';
-    middleInitial = 'J';
-
-    number_of_pencils = 10;
-    number_of_notebooks = 3;
-
-    printf("%c%c needs %d pencils, %d notebooks, and 1 lunchbox\n",
-           firstInitial, middleInitial,number_of_pencils,
-           number_of_notebooks);
-    printf("The total cost is $%.2f\n\n", number_of_pencils*pencils
-           + number_of_notebooks*notebooks + lunchbox);
-
-    //The information for the third child
-    firstInitial = 'M';
-    middleInitial = 'T';
-
-    number_of_pencils = 9;
-    number_of_notebooks = 2;
-
-    printf("%c%c needs %d pencils, %d notebooks, and 1 lunchbox\n",
-           firstInitial, middleInitial,number_of_pencils,
-           number_of_notebooks);
-    printf("The total cost is $%.2f\n",
-           number_of_pencils*pencils + number_of_notebooks*notebooks +
-           lunchbox);
 
     return 0;
-
 }
