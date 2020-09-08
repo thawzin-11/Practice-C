e3af6619bc223a45391b493831d132f272207a74
thawzin-11 <skydark.thawzintun@gmail.com>
2020-09-08 10:23:40 +0630

    Array Test

--- main.c
+++ main.c
@@ -3,9 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    printf("hello world!2\n");
-    printf("hello world!2\n");
-
+    int luckyNumber[] = {4, 8, 15, 16, 23, 43};     //char, double, float
+    luckyNumber[1] = 200;
+    printf("%d", luckyNumber[1]);
     return 0;
 }
+
+/* int arrayMain()
+{
+    int luckyNumber[20];
+    luckyNumber[1] = 90;
+    luckyNumber[0] = 80;
+    printf("%d", luckNumber[1]);
+    return 0:
+} */
