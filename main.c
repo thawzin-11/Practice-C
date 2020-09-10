05ed866bcf43ef0e42b515f6edd6800de8b97b3f
thawzin-11 <skydark.thawzintun@gmail.com>
2020-09-10 22:35:31 +0630

    Arrays Initialization

--- main.c
+++ main.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MONTHS 12
+
 int main()
 {
-    int grades[10];     // Array storing 10 values
-    int count = 10;     // Number of values to be read
-    long sum = 0;       //Sum of numbers
-    float average = 0.0f;   //Average of Numbers
+    int days[MONTHS] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int index;
 
-    printf("Enter the 10 grades:\n");      // Prompt for the input
-
-    // Read the ten numbers to be avaeraged
-    int i;
-    for (i = 0; i < count; ++i){
-        printf("%2u> ", i + 1);
-        scanf("%d", &grades[i]);    // Read a grade
-        sum += grades[i];       // Add it to sum
+    for (index = 0; index < MONTHS; index++){
+        printf("Month %d has %d days.\n", index + 1, days[index]);
     }
-
-    average = (float) sum/count;
-
-    printf("\nAverage of ten grades entered is: %.2f\n", average);
 
     return 0;
 }
