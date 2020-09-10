267cea910ff873ebfd6ebcdfc706bfbdf008710b
thawzin-11 <skydark.thawzintun@gmail.com>
2020-09-10 22:15:35 +0630

    Creating and using Arrays

--- main.c
+++ main.c
@@ -3,17 +3,24 @@
 
 int main()
 {
-    int luckyNumber [] = {4, 8, 15, 16, 23, 42};
+    int grades[10];     // Array storing 10 values
+    int count = 10;     // Number of values to be read
+    long sum = 0;       //Sum of numbers
+    float average = 0.0f;   //Average of Numbers
 
+    printf("Enter the 10 grades:\n");      // Prompt for the input
+
+    // Read the ten numbers to be avaeraged
     int i;
-    for(i = 0; i < 6; i++){
-        printf("%d\n", luckyNumber[i]);
+    for (i = 0; i < count; ++i){
+        printf("%2u> ", i + 1);
+        scanf("%d", &grades[i]);    // Read a grade
+        sum += grades[i];       // Add it to sum
     }
 
-    /* int i;
-    for(i = 1; i <= 5; i++){
-        printf("%d\n", i);
-    } */
+    average = (float) sum/count;
+
+    printf("\nAverage of ten grades entered is: %.2f\n", average);
 
     return 0;
 }
