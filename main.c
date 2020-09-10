60d48e1f506a3ee9795155a06cde0d51e496c68b
thawzin-11 <skydark.thawzintun@gmail.com>
2020-09-10 10:58:27 +0630

    Building a Guessing Game

--- main.c
+++ main.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PAYRATE 12.00
-#define TAXRATE_300 .15
-#define TAXRATE_150 .20
-#define TAXRATE_REST .25
-#define OVERTIME 40
-
 int main()
 {
-    int index = 1;
-    while (index <= 5) {
-        printf("%d\n", index);
-        index++;
+    int secretNumber = 5;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
+
+    while(guess != secretNumber && outOfGuesses == 0){
+        if(guessCount < guessLimit){
+            printf("Enter a number: ");
+            scanf("%d", &guess);
+            guessCount++;
+        } else {
+            outOfGuesses = 1;
+        }
     }
-
-    // do while loop
-
-    /* int index = 6;
-    do {
-        printf("%d", index);
-        index++;
-    } while (index <= 5); */
+    if(outOfGuesses == 1){
+        printf("Out of Guess");
+    } else {
+        printf("You Win");
+    }
 
     return 0;
 }
