b5bf3881cb8a8a93dae5d6a0cf2bcf80ed290033
thawzin-11 <skydark.thawzintun@gmail.com>
2020-09-12 16:16:46 +0630

    Function Basic

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MONTHS 12
+int multiplyTwoNumbers (int x, int y)
+{
+    int result = x * y;
+    return result;
+}
+
+
+int main(void)
+{
+    int result = 0;
+    result = multiplyTwoNumbers(10,20);
+
+    printf("result is %d\n", result);
+    return 0;
+}
+
+/* void multiplyTwoNumbers (int x, int y)
+{
+    int result = x * y;
+    printf("The product of %d is: %d\n", x,y);
+}
+
 
 int main()
 {
-    int days[MONTHS] = {31,28,31,30,31,30,31,31,30,31,30,31};
-    int index;
-
-    for (index = 0; index < MONTHS; index++){
-        printf("Month %d has %d days.\n", index + 1, days[index]);
-    }
+    multiplyTwoNumbers(10,20);
+    multiplyTwoNumbers(20,30);
+    multiplyTwoNumbers(50,2);
 
     return 0;
-}
+} */
+
+
+
